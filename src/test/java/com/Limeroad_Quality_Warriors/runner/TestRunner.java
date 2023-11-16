08a97d595e8f5b43cb5198d1a1edc1bccb77a293
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 <v.navinkumar27@gmail.com>
2023-11-16 16:23:23 +0400

    commiting runnerclass update

--- src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
+++ src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
@@ -1,8 +1,20 @@
 package com.Limeroad_Quality_Warriors.runner;
 
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 
 import com.Limeroad_Quality_Warriors.base.BaseClass;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "C:\\Users\\shubh\\eclipse-workspace\\LimeRoad_QualityWarriors\\QualityWarriors.feature", glue = "com.Limeroad_Quality_Warriors.stepdefinition", monochrome = true,
+
+                plugin={ "pretty", "html:Resources/limelogin.html",
+	                     "json:Resources/lime_login.json",
+	                     "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
 
 public class TestRunner extends BaseClass {
 	
@@ -12,5 +24,7 @@ public class TestRunner extends BaseClass {
 		getDriver("chrome");
 
 	}
+	
+	
 
 }
--- src/test/java/com/Limeroad_Quality_Warriors/stepdefinition/StepDefinition.java
+++ src/test/java/com/Limeroad_Quality_Warriors/stepdefinition/StepDefinition.java
@@ -1,5 +1,51 @@
 package com.Limeroad_Quality_Warriors.stepdefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefinition {
+
+	@When("User launch the URL {string}")
+	public void user_launch_the_url(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User Clicks on Shop Women option")
+	public void user_clicks_on_shop_women_option() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User clicks on {string} option")
+	public void user_clicks_on_option(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Earring option")
+	public void user_clicks_on_earring_option() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User selects the earring")
+	public void user_selects_the_earring() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User clicks on {string}")
+	public void user_clicks_on(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User close the browser")
+	public void user_close_the_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
