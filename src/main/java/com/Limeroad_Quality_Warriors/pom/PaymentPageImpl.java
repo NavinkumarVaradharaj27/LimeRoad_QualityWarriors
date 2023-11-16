0f07c62d4306af2439dd6cff32b0855f02cf98d7
dhanush160199 <dhanushkumar1626@gmail.com>
2023-11-16 13:38:23 +0530

    New Product updated

--- src/main/java/com/Limeroad_Quality_Warriors/pom/PaymentPageImpl.java
+++ src/main/java/com/Limeroad_Quality_Warriors/pom/PaymentPageImpl.java
@@ -1,5 +1,18 @@
 package com.Limeroad_Quality_Warriors.pom;
 
-public class PaymentPageImpl {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.Limeroad_Quality_Warriors.base.BaseClass;
+
+public class PaymentPageImpl extends BaseClass {
+	public PaymentPageImpl() {
+		PageFactory.initElements(driver,this);
+	}
+	@FindBy(id="15230812")
+	private WebElement product;
+	
+	@FindBy(id="size_19788991")
+	private WebElement selectsize;
 }
