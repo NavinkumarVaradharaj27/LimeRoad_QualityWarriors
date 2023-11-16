e097e395e6618b6c89a89477bf26ea4a8b8528aa
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 <v.navinkumar27@gmail.com>
2023-11-16 16:01:53 +0400

    added driver in runnerclass

--- src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
+++ src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
@@ -1,7 +1,16 @@
 package com.Limeroad_Quality_Warriors.runner;
 
+import org.junit.BeforeClass;
+
 import com.Limeroad_Quality_Warriors.base.BaseClass;
 
 public class TestRunner extends BaseClass {
+	
+	@BeforeClass
+	public static void browserLaunch() {
+
+		getDriver("chrome");
+
+	}
 
 }
